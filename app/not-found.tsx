c6f10c33144cb2f842c0e3c995aa275f6d1a814f
Andrei Yurkouski <emp74ark@gmail.com>
2025-09-11 22:30:41 +0200

    feat: improve 404 page UI with styled layout and "Go home" button

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,15 @@
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+
 export default function NotFound() {
   return (
-    <div>404</div>
+    <>
+      <div className="flex flex-col justify-center items-center row-span-2 gap-5">
+        <h1 className="text-9xl font-bold text-neutral-500 animate-pulse">404</h1>
+        <Button variant='outline' asChild>
+          <Link href="/">Go home</Link>
+        </Button>
+      </div>
+    </>
   );
 }
