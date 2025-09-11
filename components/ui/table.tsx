c11cd27a4103d5f0c7dcc5a84d69fff23b95c3d9
Andrei Yurkouski <emp74ark@gmail.com>
2025-09-12 00:14:23 +0200

    refactor: improve table components with dynamic column handling and utility class integration

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -1,22 +1,31 @@
 import { PropsWithChildren } from 'react';
+import { cn } from '@/lib';
 
 type TableProps = {
-  columns?: number;
+  columns: number;
 };
 
 function Table({ children }: PropsWithChildren) {
   return <div className="w-full rounded-sm overflow-hidden">{children}</div>;
 }
 
-function TableHeader({ children, columns = 2 }: PropsWithChildren<TableProps>) {
+function TableHeader({ children, columns }: PropsWithChildren<TableProps>) {
   return (
-    <div className={`grid grid-cols-${columns} bg-accent p-2`}>{children}</div>
+    <div
+      className={cn('grid grid-cols-2 bg-accent p-2', `grid-cols-${columns}`)}
+    >
+      {children}
+    </div>
   );
 }
 
-function TableRow({ children, columns = 2 }: PropsWithChildren<TableProps>) {
+function TableRow({ children, columns }: PropsWithChildren<TableProps>) {
   return (
-    <div className={`grid grid-cols-${columns} p-2 border-b-1`}>{children}</div>
+    <div
+      className={cn('grid grid-cols-2 p-2 border-b-1', `grid-cols-${columns}`)}
+    >
+      {children}
+    </div>
   );
 }
 
