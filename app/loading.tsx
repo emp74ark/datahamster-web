d603e43d8569d17b980a1105968b52e32dfe9a2b
Andrei Yurkouski <emp74ark@gmail.com>
2025-09-11 22:30:37 +0200

    feat: improve loading screen UI with spinner and backdrop styling

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,7 @@
 export default function Loading() {
   return (
-    <div>Loading</div>
+    <div className="fixed w-screen h-screen inset-0 bg-neutral-800/80 flex justify-center items-center backdrop-blur-xs">
+      <div className="rounded-full w-10 h-10 border-t-4 border-r-4 border-amber-800 animate-spin"></div>
+    </div>
   );
 }
