d7e6e1ae938863052e6c253490aa284b56648bce
Andrei Yurkouski <emp74ark@gmail.com>
2025-09-17 14:47:23 +0200

    fix: improve action list responsiveness and truncate overflow text

--- app/(authorized)/sources/[sourceId]/page.tsx
+++ app/(authorized)/sources/[sourceId]/page.tsx
@@ -28,20 +28,27 @@ export default async function Page(props: PageProps) {
       </Dialog>
       <p className="text-neutral-500 mb-5">{source?.description}</p>
       <h3 className="text-lg">Actions</h3>
-      <ul className="sticky top-2 flex flex-row gap-3 md:flex-col">
+      <ul className="sticky top-2 flex flex-row gap-3 md:flex-col flex-wrap">
         {source?.actions?.map(({ id, name, publicId }) => (
           <li
             key={id}
-            className="bg-accent p-2 rounded-sm my-2 hover:bg-accent/80 relative"
+            className="bg-accent p-2 rounded-sm my-2 hover:bg-accent/80 relative flex-1/3 md:flex-1"
           >
             <DeleteButton entity="action" id={id} />
             <Link
               className="text-lg"
               href={`/sources/${sourceId}?action=${id}`}
             >
-              {name}
+              <p
+                title={name}
+                className="w-[15ch] overflow-hidden text-ellipsis whitespace-nowrap"
+              >
+                {name}
+              </p>
               <p className="text-xs text-neutral-500/50">Public ID:</p>
-              <p className="text-xs text-neutral-500/50">{publicId}</p>
+              <p className="text-xs text-neutral-500/50 overflow-hidden w-[25ch] text-ellipsis whitespace-nowrap">
+                {publicId}
+              </p>
             </Link>
           </li>
         ))}
