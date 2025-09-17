490c7378ba58a6f65abccd70a0a43dac2b5560c1
Andrei Yurkouski <emp74ark@gmail.com>
2025-09-17 10:24:04 +0200

    fix: responsive layout for sources and actions

--- app/(authorized)/sources/[sourceId]/layout.tsx
+++ app/(authorized)/sources/[sourceId]/layout.tsx
@@ -8,8 +8,8 @@ type ActionLayoutProps = {
 
 export default function Layout({ children, actions }: ActionLayoutProps) {
   return (
-    <section className="@container flex flex-row gap-5 w-full">
-      <div className="bg-accent/50 px-4 py-5 rounded-md w-1/3">{children}</div>
+    <section className="@container flex flex-col md:flex-row gap-5 w-full">
+      <div className="bg-accent/50 px-4 py-5 rounded-md md:w-1/3">{children}</div>
       <Suspense fallback={<Loading />}>
         <div className="w-full">{actions}</div>
       </Suspense>
--- app/(authorized)/sources/[sourceId]/page.tsx
+++ app/(authorized)/sources/[sourceId]/page.tsx
@@ -28,7 +28,7 @@ export default async function Page(props: PageProps) {
       </Dialog>
       <p className="text-neutral-500 mb-5">{source?.description}</p>
       <h3 className="text-lg">Actions</h3>
-      <ul className="sticky top-2">
+      <ul className="sticky top-2 flex flex-row gap-3 md:flex-col">
         {source?.actions?.map(({ id, name, publicId }) => (
           <li
             key={id}
